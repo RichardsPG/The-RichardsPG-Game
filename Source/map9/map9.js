31332fbe4210b1de80d75064be527475446892b7
ansonmouse <ansonmouse@me.com>
2015-11-05 21:24:48 +0800

    Map9

--- Source/map9/map9.js
+++ Source/map9/map9.js
@@ -1,8 +1,26 @@
 
-_map_event[8] = [];
+_map_event[8] = [
+    163, 164, 165, 166,
+    183, 184, 185, 186,
+    203, 204, 205, 206,
+    223, 224, 225, 226
+];
 
 function initalMap9(){
+    if (_map_deco[1] == 1) {
+
+    }
 }
 
 function startMap9Event(x,y){
+    alert(x + ', ' + y);
+    endEvent();
+}
+
+function markSelectedPosition(x, y) {
+
+}
+
+function checkMatch(x, y) {
+
 }
